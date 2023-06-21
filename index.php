8e71947482bb4abc95e2705ebef1c448339dc062
DaveKnud <daveknud@outlook.com>
2023-06-21 16:58:09 +0200

    new commit

--- index.php
+++ index.php
@@ -16,19 +16,21 @@
 
     <?php
         $passwordUtente = $_GET['text'];
-        var_dump($passwordUtente);
     ?>
 
     <?php
         //FUNZIONE GENERA PASSWORD
-        function randomPassword() {
-            $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-            for ($i = 0; $i < 8; $i++) {
-                $n = rand(0, count($alphabet)-1);
-                $pass .= $alphabet[$n];
+       function randomPassword() {
+            $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            $count = mb_strlen($alphabet);
+
+            for ($i = 0, $result = ''; $i < $length; $i++) {
+                $index = rand(0, $count - 1);
+                $result .= mb_substr($alphabet, $index, 1);
             }
-            return $pass;
-        };
+
+            return $result;
+}
 
         echo randomPassword($passwordUtente);
         var_dump($passwordUtente);
