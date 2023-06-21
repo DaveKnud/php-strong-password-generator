946a0a42f952f4d2ff1224514edadb09b0ab0787
DaveKnud <daveknud@outlook.com>
2023-06-21 16:24:38 +0200

    new push

--- index.php
+++ index.php
@@ -25,12 +25,13 @@
             $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
             for ($i = 0; $i < 8; $i++) {
                 $n = rand(0, count($alphabet)-1);
-                $pass[$i] = $alphabet[$n];
+                $pass .= $alphabet[$n];
             }
             return $pass;
         };
 
         echo randomPassword($passwordUtente);
+        var_dump($passwordUtente);
     ?>
 
     <?php
